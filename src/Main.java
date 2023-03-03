3808cc8a84373bf4df6653b72c41bfb8c9d187f6
Jodhveer Atwal <atwaljodhveer@gmail.com>
2023-03-02 16:32:39 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner UserIn = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle: ");
+        int width = UserIn.nextInt();
+        System.out.println("Enter the length of the rectangle: ");
+        int length = UserIn.nextInt();
+
+
+    }
+}
+
+class Rectangle{
+    int length;
+    int width;
+    public static void calculateArea(int l,int w){
+
+    }
+}
